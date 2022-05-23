1ba253bf59f58b469a4dbbfd0b52922cd5bc725a
django_freeman <o.tampodo.t.18@gmail.com>
2022-05-23 20:36:22 +0900

    sample route追加

--- server/server.js
+++ server/server.js
@@ -6,13 +6,23 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // restaurant取得Route（サンプル）
-app.get("/api/getRestaurants", (req, res) => {
+app.get("/api/v1/restaurants", (req, res) => {
     res.status(200).json({
         status: "success",
         data: {
             restaurant: ["yoshinoya", "sukiya", "matsuya"]
         }
     })})
+
+// restaurant単一取得（サンプル）
+app.get("/api/v1/restaurants/:restaurantid", (req, res) => {
+    console.log(req.params);
+})
+
+// restaurant作成（サンプル）
+app.post("/api/v1/restaurants", (req, res) => {
+    console.log(req);
+})
 
 app.listen(port, () => {
     console.log(`server start on port ${port}`);
