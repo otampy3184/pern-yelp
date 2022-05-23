8e9c747f77d68a0ca2edab4d791a9efeac565958
django_freeman <o.tampodo.t.18@gmail.com>
2022-05-23 21:26:09 +0900

    review追加用のROUTE

--- server/server.js
+++ server/server.js
@@ -120,8 +120,25 @@ app.delete("/api/v1/restaurants/:id", async (req, res) => {
     } catch (err) {
         console.log(err)
     }
-
 })
+
+app.post("/api/v1/restaurants/:id/addReview", async (req, res) => {
+    try {
+      const newReview = await db.query(
+        "INSERT INTO reviews (restaurant_id, name, review, rating) values ($1, $2, $3, $4) returning *;",
+        [req.params.id, req.body.name, req.body.review, req.body.rating]
+      );
+      console.log(newReview);
+      res.status(201).json({
+        status: "success",
+        data: {
+          review: newReview.rows[0],
+        },
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  });
 
 app.listen(port, () => {
     console.log(`server start on port ${port}`);
