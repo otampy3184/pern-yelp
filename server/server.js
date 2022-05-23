f46e5354eea7e0aeec780975c61f2504b498bce7
django_freeman <o.tampodo.t.18@gmail.com>
2022-05-23 20:56:18 +0900

    u d 追加

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 require('dotenv').config(); 
-const express = require('express');
+const express = require('express')
+const cors = require("cors");
+const db = require("./db");
+
+const morgan = require("morgan");
 
 const app = express();
+
+// オリジン間リソース共有ソース
+app.use(cors());
+// json用のミドルウェア
+app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
@@ -15,13 +24,45 @@ app.get("/api/v1/restaurants", (req, res) => {
     })})
 
 // restaurant単一取得（サンプル）
-app.get("/api/v1/restaurants/:restaurantid", (req, res) => {
+app.get("/api/v1/restaurants/:id", (req, res) => {
     console.log(req.params);
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurant: "sukiya"
+        }
+    })
 })
 
 // restaurant作成（サンプル）
 app.post("/api/v1/restaurants", (req, res) => {
     console.log(req);
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurant: "sukiya"
+        }
+    })
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurant: "sukiya"
+        }
+    })
+})
+
+// restaurant更新（サンプル）
+app.post("/api/v1/restaurants/:id", (req, res) => {
+    console.log(req.params.id);
+    console.log(req.body);
+})
+
+// restaurant削除（サンプル）
+app.delete("/api/v1/restaurants/:id", (req, res) => {
+    res.status(204).json({
+        status: "sucess"
+    })
 })
 
 app.listen(port, () => {
