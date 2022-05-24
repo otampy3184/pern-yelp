fd35706db5feec0cc6426e91c65665253e7312d3
django_freeman <o.tampodo.t.18@gmail.com>
2022-05-24 18:13:13 +0900

    reactの基本ファイルを追加

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import UpdatePage from "./routes/UpdatePage";
+import RestaurantDetailPage from "./routes/RestaurantDetailPage";
+import { RestaurantsContextProvider } from "./context/RestaurantsContext";
+
+const App = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default App,
