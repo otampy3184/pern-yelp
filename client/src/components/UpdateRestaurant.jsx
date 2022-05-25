00c67d679f0b23685e26137b10e64a18ae3194b6
django_freeman <o.tampodo.t.18@gmail.com>
2022-05-25 23:20:01 +0900

    updateRestaurant追加

---
+++ client/src/components/UpdateRestaurant.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useContext, useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { RestaurantsContext } from "../context/RestaurantsContext";
+import RestaurantFinder from "../apis/RestaurantFinder";
+
+const UpdateRestaurant = (props) => {
+    const { id } = useParams();
+    let history = useHistory();
+    const { restaurants } = useContext(RestaurantsContext);
+    const [name, setName] = useState("");
+    const [location, setLocation] = useState("");
+    const [priceRange, setPriceRange] = useState("");
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await RestaurantFinder.get(`/${id}`);
+            console.log(response.data.data);
+            setName(response.data.data.restaurant.name);
+            setLocation(response.data.data.restaurant.location);
+            setPriceRange(response.data.data.restaurant.price_range);
+        };
+      
+        fetchData();
+    }, []);
+
+    // TODO: complete handleSubmit
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    }
+
+    // TODO: complete return
+    return (
+        <div></div>
+    )
+}
