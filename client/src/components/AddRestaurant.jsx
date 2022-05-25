bebfda98b76febdc6b571f77d40e1b5656cf332c
django_freeman <o.tampodo.t.18@gmail.com>
2022-05-25 22:42:40 +0900

    レストラン追加用のコンポーネント作成

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -1,3 +1,23 @@
 import React, { useState, useContext } from "react";
 import RestaurantFinder from "../apis/RestaurantFinder";
-import { }
+import { RestaurantsContext } from "../context/RestaurantsContext";
+import { useHistory } from "react-router-dom";
+
+const RestaurantList = (props) => {
+    const {restaurants, setRestaurants } = useContext(RestaurantsContext);
+    const [name, setName] = useState("");
+    const [location, setLocation] = useState("");
+    const [priceRange, setPriceRange] = useState("Price Range");
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+
+        } catch (err){
+            console.log(err);
+        }
+    };
+    return (
+        <div></div>
+    )
+}
