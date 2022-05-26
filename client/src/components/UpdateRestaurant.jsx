66fd1d1b7c4dab97d4a644bfa285c8fa1ed8d88e
django_freeman <o.tampodo.t.18@gmail.com>
2022-05-26 23:38:39 +0900

    updaterestaurantのhandlerとreturn作成

--- client/src/components/UpdateRestaurant.jsx
+++ client/src/components/UpdateRestaurant.jsx
@@ -23,13 +23,62 @@ const UpdateRestaurant = (props) => {
         fetchData();
     }, []);
 
-    // TODO: complete handleSubmit
     const handleSubmit = async (e) => {
         e.preventDefault();
+        try {
+            const res = await RestaurantFinder.put(`/{$id}`, {
+                name,
+                location,
+                priceRange: priceRange,
+            });
+            history.push("/");
+        } catch (err) {
+            console.log(err);
+        }
     }
 
-    // TODO: complete return
     return (
-        <div></div>
+        <div>
+            <form action="">
+            <div className="form-group">
+                <label htmlFor="name">Name</label>
+                <input
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                id="name"
+                className="form-control"
+                type="text"
+                />
+            </div>
+    
+            <div className="form-group">
+                <label htmlFor="location">Location</label>
+                <input
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}
+                id="location"
+                className="form-control"
+                type="text"
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="price_range">Price Range</label>
+                <input
+                value={priceRange}
+                onChange={(e) => setPriceRange(e.target.value)}
+                id="price_range"
+                className="form-control"
+                type="number"
+                />
+            </div>
+            <button
+                type="submit"
+                onClick={handleSubmit}
+                className="btn btn-primary"
+            >
+                Submit
+            </button>
+            </form>
+        </div>
     )
 }
