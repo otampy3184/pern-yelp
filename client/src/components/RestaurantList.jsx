7e5a0edc5483e3df3650c4e567f1c86e6ce358b7
django_freeman <o.tampodo.t.18@gmail.com>
2022-05-26 23:27:34 +0900

    restaurantListのhandle系を作成

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -21,30 +21,77 @@ const RestaurantList = (props) => {
         fetchData();
     }, []);
 
-    // TODO: complete handleDelete
     const handleDelete = async (e, id) => {
         e.stopPropagation();
         try {
-
+            const res = await RestaurantFinder.delete(`/{$id}`);
+            setRestaurants(
+                restaurants.filter((restaurant) => {
+                    return restaurant.id !== id;
+                })
+            );
         } catch (err) {
             console.log(err)
         }
     }
 
-    // TODO: complete handleUpdate
     const handleUpdate = (e, id) => {
-
+        e.stopPropagation();
+        history.push(`/restaurants/{$id}/update`);
     }
 
-    // TODO: complete handleRestaurantSelect
     const handleRestaurantSelect = (id) => {
-
+        history.push(`/restaurants/${id}`);
     }
 
-    // TODO: complete return
     return (
-        <div></div>
-    )
+        <div className="list-group">
+        <table className="table table-hover table-dark">
+          <thead>
+            <tr className="bg-primary">
+              <th scope="col">Restaurant</th>
+              <th scope="col">Location</th>
+              <th scope="col">Price Range</th>
+              <th scope="col">Ratings</th>
+              <th scope="col">Edit</th>
+              <th scope="col">Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {restaurants &&
+              restaurants.map((restaurant) => {
+                return (
+                  <tr
+                    onClick={() => handleRestaurantSelect(restaurant.id)}
+                    key={restaurant.id}
+                  >
+                    <td>{restaurant.name}</td>
+                    <td>{restaurant.location}</td>
+                    <td>{"$".repeat(restaurant.price_range)}</td>
+                    <td>{renderRating(restaurant)}</td>
+                    <td>
+                      <button
+                        onClick={(e) => handleUpdate(e, restaurant.id)}
+                        className="btn btn-warning"
+                      >
+                        Update
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        onClick={(e) => handleDelete(e, restaurant.id)}
+                        className="btn btn-danger"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            {}
+          </tbody>
+        </table>
+      </div>    )
 }
 
 export default RestaurantList;
